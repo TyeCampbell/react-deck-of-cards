2ba8a24d9bb81ea91f99fea08488cd5bdb73aaad
Tye Campbell <tylerjoelcampbell@gmail.com>
2020-02-06 05:49:25 -0600

    added Card and CardTable components

---
+++ src/CardTable.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class CardTable extends Component {
+    constructor(props) {
+        super(props); 
+    }
+
+    render() {
+
+        return(
+
+        )
+    }
+
+}
+
+export default CardTable; 
