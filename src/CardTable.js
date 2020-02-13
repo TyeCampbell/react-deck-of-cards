63539326ef9bacb8a35bc475cace2011ac44cea5
Tye Campbell <tylerjoelcampbell@gmail.com>
2020-02-13 05:46:45 -0600

    fixed logic for scoring ACE cards either 1 or 11. Also now using conditional operator on the buttons to use different functions based on the state of the game

--- src/CardTable.js
+++ src/CardTable.js
@@ -48,6 +48,8 @@ class CardTable extends Component {
 
         this.state.cardsDrawn.forEach(element => {
             
+            console.log("Hand total before conditional: " + this.state.handTotal)
+            console.log("Is this an ace? " + element.value === "ACE")
 
             if (element.value === "KING") {
                 total += 10;
@@ -56,10 +58,10 @@ class CardTable extends Component {
             } else if (element.value === "JACK") {
                 total += 10;
             } else if ( element.value === "ACE") {
-                if (this.state.handTotal < 10) {
-                    total += 11;
+                if (this.state.handTotal + 11 > 21) {
+                    total += 1;
                 } else {
-                    total += 1
+                    total += 11
                 }    
             } else {
                 total += parseInt(element.value);
@@ -67,6 +69,9 @@ class CardTable extends Component {
         })
         //return total;
         this.setState({handTotal: total})
+
+        console.log("Hand total after conditional & setState: " + this.state.handTotal)
+
     }
 
     holdCards() {
@@ -79,16 +84,26 @@ class CardTable extends Component {
             <Card key={item.value+item.suit} image={item.image} alt={`${item.value} of ${item.suit}`}/> 
         ))
 
+        let gameStatus = ""
+
+        if (this.state.handTotal === 21) {
+            gameStatus = "21! You win!"
+        }
+
+        if (this.state.handTotal > 21) {
+            gameStatus = "BUST! You lose!"
+        }
+
         return (
            <div>
                <section className='CardTable-upper'>
-                    {this.state.handTotal > 21 ? <h1 className='CardTable-lose'>BUST! You lose!</h1> : ""}
+                <p className='CardTable-lose'>{gameStatus}</p>
                     {cards}
                </section>
                 <section className='CardTable-lower'>
                     <p>Hand Total: {this.state.handTotal}</p>
-                    <button onClick={this.drawHand}>Draw Hand</button>
-                    <button disabled={this.state.handTotal > 21} onClick={this.getCard}>Hit Me!</button>
+                    <button disabled={this.state.handTotal > 21 || this.state.handTotal === 21}onClick={this.state.handTotal === 0 ? this.drawHand : this.getCard}>{this.state.handTotal === 0 ? "Draw Hand" : "Hit Me!"}</button>
+                    {/* <button disabled={this.state.handTotal > 21 || this.state.handTotal === 21} onClick={this.getCard}>Hit Me!</button> */}
                     <button onClick={this.holdCards}>Hold</button>
                 </section>
            </div>
